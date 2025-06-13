c7a73a4405ef898ec9db2635e16ad035fdc2411d
ericnunes30 <121236886+ericnunes30@users.noreply.github.com>
2025-06-13 08:31:23 -0300

    refactor(frontend): reorganizar rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,76 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// Dashboard (Index) removido conforme solicitado
-import Login from "./pages/Login";
-import Projects from "./pages/Projects";
-import ProjectView from "./pages/ProjectView";
-import Tasks from "./pages/Tasks";
-import Teams from "./pages/Teams";
-import TaskDetails from "./pages/TaskDetails";
-import NotFound from "./pages/NotFound";
-// Calendário removido conforme solicitado
-import Settings from "./pages/Settings";
-import Users from "./pages/Users";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import { AuthProvider } from "./contexts/AuthContext";
-
-// Componente para envolver as rotas com o AuthProvider
-const AppRoutes = () => {
-  return (
-    <Routes>
-      {/* Rota pública */}
-      <Route path="/login" element={<Login />} />
-
-      {/* Rotas protegidas */}
-      {/* Rota principal redirecionada para Projetos */}
-      <Route path="/" element={
-        <ProtectedRoute>
-          <Projects />
-        </ProtectedRoute>
-      } />
-      <Route path="/projects" element={
-        <ProtectedRoute>
-          <Projects />
-        </ProtectedRoute>
-      } />
-      <Route path="/projects/:projectId" element={
-        <ProtectedRoute>
-          <ProjectView />
-        </ProtectedRoute>
-      } />
-      <Route path="/tasks" element={
-        <ProtectedRoute>
-          <Tasks />
-        </ProtectedRoute>
-      } />
-      <Route path="/teams" element={
-        <ProtectedRoute>
-          <Teams />
-        </ProtectedRoute>
-      } />
-      <Route path="/tasks/:taskId" element={
-        <ProtectedRoute>
-          <TaskDetails />
-        </ProtectedRoute>
-      } />
-      {/* Rota de Calendário removida conforme solicitado */}
-      <Route path="/settings" element={
-        <ProtectedRoute>
-          <Settings />
-        </ProtectedRoute>
-      } />
-      <Route path="/users" element={
-        <ProtectedRoute>
-          <Users />
-        </ProtectedRoute>
-      } />
-      
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-};
+import AppRoutes from "@/routes";
 
 const App = () => {
   return (
@@ -81,11 +12,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <AuthProvider>
-            <AppRoutes />
-          </AuthProvider>
-        </BrowserRouter>
+        <AppRoutes />
       </TooltipProvider>
     </React.StrictMode>
   );
