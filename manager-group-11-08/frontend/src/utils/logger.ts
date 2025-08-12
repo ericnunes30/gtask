b00ebd8aa24a39828dbad9d4083bacacfa4abe09
ericnunes30 <121236886+ericnunes30@users.noreply.github.com>
2025-08-12 12:43:34 -0300

    Delete manager-group-11-08 directory

--- manager-group-11-08/frontend/src/utils/logger.ts
+++
@@ -1,8 +0,0 @@
-const logger = {
-  info: (...args: unknown[]) => console.info(...args),
-  error: (...args: unknown[]) => console.error(...args),
-  warn: (...args: unknown[]) => console.warn(...args),
-  debug: (...args: unknown[]) => console.debug(...args),
-};
-
-export default logger;
